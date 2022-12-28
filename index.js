469909a2d87b34652ebd0f40864fe48efa00cf77
shohruh973 <abdumannobovshohruh973@gmail.com>
2022-12-28 16:49:48 +0500

    Java script sh

---
+++ index.js
@@ -0,0 +1,18 @@
+// let name = "Shohruh"
+// let age = 18
+// console.log("mening ismim " + name +" yoshim "  + age + " da ");
+
+
+
+// let a ="5"
+// let b ="5"
+
+
+// console.log(a+b);
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a*b)
+
+let a = prompt("Birinchi sonni kiriting")
+let b = prompt("ikkinchi sonni kiriting")
+console.log(+a + +b)
